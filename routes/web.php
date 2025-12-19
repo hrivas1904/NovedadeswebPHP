5dcd49db4a3a381ab7684f68d52198ced6988d77
hrivas465 <hrivas0260@gmail.com>
2025-12-19 00:37:15 -0300

    inicio

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PersonalController;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])
+    ->name('index');
+
+
+Route::get('/', [PersonalController::class, 'nominaPersonal'])
+    ->name('nominaPersonal');
