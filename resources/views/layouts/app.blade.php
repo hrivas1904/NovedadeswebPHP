ccb7a2c5b4c6aa9978ee8c7a036f9f4ea9b16ce2
hrivas1904 <hrivas1904@gmail.com>
2025-12-26 11:01:44 -0300

    listo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,9 +93,6 @@
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
 
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-
     <script src="{{ asset('js/script.js') }}"></script>
 
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
