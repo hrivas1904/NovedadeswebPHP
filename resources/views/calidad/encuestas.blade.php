ac3553a2340050d8e395d8752a9396ef541f3b5c
hrivas1904 <hrivas1904@gmail.com>
2026-04-08 13:33:14 -0300

    encuestas view

--- resources/views/calidad/encuestas.blade.php
+++ resources/views/calidad/encuestas.blade.php
@@ -11,20 +11,16 @@
 
         <div class="row d-flex align-item-center justify-content-start">
             <div class="col-auto">
-                <input type="file" id="excelFile" accept=".xlsx,.xls,.csv" class="btn-primario">
-                <button id="btnImportar" class="btn-primario">
-                    Importar Excel
-                </button>
-                <button id="btnProcesar" class="btn btn-primary">
-                    Procesar estructura
-                </button>
-
-            </div>
-            <div class="col-auto">
                 <select id="selectTipoEncuesta" class="form-control"></select>
             </div>
+            <div class="col-auto">
+                <input type="file" id="excelFile" accept=".xlsx,.xls,.csv" class="btn-primario">
+                <button id="btnImportar" type="button" class="btn-primario">
+                    Importar Excel
+                </button>
+            </div>            
             <div class="col-2">
-                <button id="btnAnalizarArchivo" class="btn btn-primario">
+                <button type="button" id="btnAnalizar" class="btn btn-primario">
                     Analizar
                 </button>
             </div>
@@ -32,7 +28,6 @@
 
         <div class="container-fluid my-3 empleado-box p-3">
             <h5 class="fw-bold mb-3">Previsualización del archivo</h5>
-            <form id="formGuardarResultados">
                 <div class="row d-flex gap-3 mx-auto my-4">
                     <table id="tablaExcel" class="table table-striped table-bordered table-hover align-middle nowrap">
                         <thead>
@@ -41,7 +36,6 @@
                         </tbody>
                     </table>
                 </div>
-            </form>
         </div>
 
     </div>
